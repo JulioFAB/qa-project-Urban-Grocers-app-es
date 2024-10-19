e113c56a5459ee45f69b759017e2fda0680843ac
unknown <fuyo.161@gmail.com>
2024-10-18 23:11:56 -0600

    Mensaje de confirmación para el submódulo

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-287781d0-1f6a-4440-9a77-a685184baa3a.containerhub.tripleten-services.com"# Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-7f012c7e-7506-4c4d-bb8d-88601c1e3606.containerhub.tripleten-services.com"# Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users/" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH =  "/api/v1/kits"# Almacena la ruta para crear un kit en esta variable
 
